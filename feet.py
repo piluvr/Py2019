0a2e862ec59289ba55d287d2a1f0375031aa211e
Piluvr <kenneths516@gmail.com>
2019-10-04 13:36:46 -0700

    AA

--- feet.py
+++ feet.py
@@ -1,10 +1,8 @@
 # your code goes here
 feet = int(input("Enter the Feet for the first piece of fabric:"))
 inches = int(input("Enter the Inches for the first piece of fabric:"))
-
 feet2 = int(input("Enter the Feet for the second piece of fabric:"))
 inches2 = int(input("Enter the Inches for the second piece of fabric:"))
-
-final1= ((feet + feet2)+(((inches1+inches2)%12))
-final2= ((inches1 + inches2)-(((inches1+inches2)%12))*12)
-print("Feet: " + final1 + " Inches: " + final2)
+finalA= str((feet + feet2)+((int((inches+inches2)/12))))
+finalB= str(((((inches+inches2)%12))))
+print("Feet: " + finalA + " Inches: " + finalB)
